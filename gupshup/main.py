d356b1389fd0daec4d705f0d315bd1168653150b
kraanzu <kraanzu@gmail.com>
2022-02-17 09:13:33 +0530

    Minor changes

--- gupshup/main.py
+++ gupshup/main.py
@@ -1,10 +1,11 @@
 import sys
-from gupshup.ui import Tui
-from gupshup.src.server import Server
+from .ui import Tui
+from .src.server import Server
 
-if sys.argv[1].lower() == "server":
-    server = Server()
-    server.start_connection()
-else:
-    app = Tui()
-    app.run()
+def main():
+    if sys.argv[1].lower() == "server":
+        server = Server()
+        server.start_connection()
+    else:
+        app = Tui()
+        app.run()
