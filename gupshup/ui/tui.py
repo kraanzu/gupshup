bfaf5282a1372cc7279c181fec77a3680dfa24c7
kraanzu <kraanzu@gmail.com>
2022-03-09 08:52:35 +0530

    Add try block for message clicks
    TODO: Do somthing about messages

--- gupshup/ui/tui.py
+++ gupshup/ui/tui.py
@@ -41,16 +41,10 @@ class Tui(App):
         # sets up default screen
         self.current_house = "HOME"
         self.current_room = "general"
-        self.current_screen = (
-            f"{self.current_house}/{self.current_room}"
-        )
+        self.current_screen = f"{self.current_house}/{self.current_room}"
 
-        self.member_lists: dict[
-            str, MemberList
-        ] = defaultdict(MemberList)
-        self.member_scrolls: dict[
-            str, ScrollView
-        ] = defaultdict(ScrollView)
+        self.member_lists: dict[str, MemberList] = defaultdict(MemberList)
+        self.member_scrolls: dict[str, ScrollView] = defaultdict(ScrollView)
 
         # some keybindings
         await self.bind(
@@ -120,20 +114,12 @@ class Tui(App):
             )
 
         for name, rank in house.ranks.items():
-            await self.member_lists[house.name].add_rank(
-                name
-            )
-            self.member_lists[
-                house.name
-            ].change_data_parent(name, "color", rank.color)
-            self.member_lists[
-                house.name
-            ].change_data_parent(name, "icon", rank.icon)
+            await self.member_lists[house.name].add_rank(name)
+            self.member_lists[house.name].change_data_parent(name, "color", rank.color)
+            self.member_lists[house.name].change_data_parent(name, "icon", rank.icon)
 
         for name, rank in house.member_rank.items():
-            await self.member_lists[
-                house.name
-            ].add_user_to_rank(rank, name)
+            await self.member_lists[house.name].add_user_to_rank(rank, name)
 
     async def perform_connection_disable(self, *_) -> None:
         self.headbar.status = "ﮡ Can't connect"
@@ -141,158 +127,96 @@ class Tui(App):
     async def perform_connection_enable(self, *_) -> None:
         self.headbar.status = " Online"
 
-    async def perform_push_text(
-        self, message: Message, local=False
-    ) -> None:
+    async def perform_push_text(self, message: Message, local=False) -> None:
         """
         Performs adding all the text messages to their respective locations
         """
         screen = f"{message.house}/{message.room}"
         await self.chat_screen[screen].push_text(message)
 
-        if (
-            not local
-        ):  # check if local/offline data is not being pushed
+        if not local:  # check if local/offline data is not being pushed
             if self.current_screen == screen:
                 # await self.chat_scroll.update(
                 #     self.chat_screen[screen].chats, home=False
                 # )
-                self.chat_screen[
-                    self.current_screen
-                ].refresh(layout=True)
+                self.chat_screen[self.current_screen].refresh(layout=True)
                 # self.chat_scroll.animate(
                 #     "y",
                 #     self.chat_scroll.max_scroll_y + self.chat_scroll.y,
                 #     easing="none",
                 # )
             else:
-                if not self.house_tree.is_room_silent(
-                    message.house, message.room
-                ):
+                if not self.house_tree.is_room_silent(message.house, message.room):
                     self.console.bell()
 
-                self.house_tree.increase_pending(
-                    message.house, message.room
-                )
+                self.house_tree.increase_pending(message.house, message.room)
 
-    async def perform_add_room(
-        self, message: Message
-    ) -> None:
-        await self.house_tree.add_room(
-            message.house, message.data["room"]
-        )
+    async def perform_add_room(self, message: Message) -> None:
+        await self.house_tree.add_room(message.house, message.data["room"])
 
-    async def perform_clear_chat(
-        self, message: Message
-    ) -> None:
+    async def perform_clear_chat(self, message: Message) -> None:
         screen = f"{message.house}/{message.room}"
         await self.chat_screen[screen].clear_chat()
         if screen == self.current_screen:
             self.chat_screen[screen].refresh()
 
-    async def perform_del_room(
-        self, message: Message
-    ) -> None:
+    async def perform_del_room(self, message: Message) -> None:
         screen = f"{message.house}/{message.room}"
 
-        self.house_tree.del_room(
-            message.house, message.room
-        )
+        self.house_tree.del_room(message.house, message.room)
         self.chat_screen[screen].chats = ""
         if self.current_screen == screen:
-            await self.update_chat_screen(
-                message.house, "general"
-            )
+            await self.update_chat_screen(message.house, "general")
 
-    async def perform_del_house(
-        self, message: Message
-    ) -> None:
+    async def perform_del_house(self, message: Message) -> None:
         self.house_tree.del_house(message.house)
         await self.update_chat_screen("HOME", "general")
 
-    async def perform_add_rank(
-        self, message: Message
-    ) -> None:
-        await self.member_lists[message.house].add_rank(
-            message.data["rank"]
-        )
+    async def perform_add_rank(self, message: Message) -> None:
+        await self.member_lists[message.house].add_rank(message.data["rank"])
 
-    async def perform_del_rank(
-        self, message: Message
-    ) -> None:
-        await self.member_lists[message.house].del_rank(
-            message.data["rank"]
-        )
+    async def perform_del_rank(self, message: Message) -> None:
+        await self.member_lists[message.house].del_rank(message.data["rank"])
 
-    async def perform_add_user_rank(
-        self, message: Message
-    ) -> None:
-        await self.member_lists[
-            message.house
-        ].add_user_to_rank(
+    async def perform_add_user_rank(self, message: Message) -> None:
+        await self.member_lists[message.house].add_user_to_rank(
             message.data["rank"], message.data["user"]
         )
 
-    async def perform_del_user_rank(
-        self, message: Message
-    ) -> None:
-        await self.member_lists[
-            message.house
-        ].del_from_rank(
+    async def perform_del_user_rank(self, message: Message) -> None:
+        await self.member_lists[message.house].del_from_rank(
             message.data["rank"], message.data["user"]
         )
 
-    async def perform_change_rank_color(
-        self, message: Message
-    ) -> None:
-        await self.member_lists[
-            message.house
-        ].change_rank_data(
+    async def perform_change_rank_color(self, message: Message) -> None:
+        await self.member_lists[message.house].change_rank_data(
             message.data["rank"],
             "color",
             message.data["color"],
         )
 
-    async def perform_change_rank_name(
-        self, message: Message
-    ) -> None:
-        await self.member_lists[
-            message.house
-        ].change_rank_name(
+    async def perform_change_rank_name(self, message: Message) -> None:
+        await self.member_lists[message.house].change_rank_name(
             message.data["rank"], message.data["name"]
         )
 
-    async def perform_change_rank_icon(
-        self, message: Message
-    ) -> None:
-        await self.member_lists[
-            message.house
-        ].change_rank_data(
+    async def perform_change_rank_icon(self, message: Message) -> None:
+        await self.member_lists[message.house].change_rank_data(
             message.data["rank"],
             "icon",
             message.data["icon"],
         )
 
-    async def perform_change_room_name(
-        self, message: Message
-    ) -> None:
+    async def perform_change_room_name(self, message: Message) -> None:
         name = message.data["name"]
-        self.house_tree.change_room_name(
-            message.house, message.room, name
-        )
-        self.chat_screen[
-            f"{message.house}/{name}"
-        ] = self.chat_screen[
+        self.house_tree.change_room_name(message.house, message.room, name)
+        self.chat_screen[f"{message.house}/{name}"] = self.chat_screen[
             f"{message.house}/{message.room}"
         ]
-        del self.chat_screen[
-            f"{message.house}/{message.room}"
-        ]
+        del self.chat_screen[f"{message.house}/{message.room}"]
         await self.update_chat_screen(message.house, name)
 
-    async def perform_change_room_icon(
-        self, message: Message
-    ) -> None:
+    async def perform_change_room_icon(self, message: Message) -> None:
         self.house_tree.change_room_data(
             message.house,
             message.room,
@@ -300,16 +224,10 @@ class Tui(App):
             message.data["icon"],
         )
 
-    async def perform_toggle_silent(
-        self, message: Message
-    ) -> None:
-        self.house_tree.toggle_silent(
-            message.house, message.room
-        )
+    async def perform_toggle_silent(self, message: Message) -> None:
+        self.house_tree.toggle_silent(message.house, message.room)
 
-    async def execute_message(
-        self, message: Message
-    ) -> None:
+    async def execute_message(self, message: Message) -> None:
         """
         Executes the messages recieved from the server
         """
@@ -335,9 +253,7 @@ class Tui(App):
 
         self.title = "Welcome to Gupshup"
         self.headbar = Headbar()
-        self.input_box = TextInput(
-            placeholder="Say something here..."
-        )
+        self.input_box = TextInput(placeholder="Say something here...")
 
         self.banner = Banner()
         self.chat_screen = defaultdict(ChatScreen)
@@ -348,14 +264,12 @@ class Tui(App):
         await self.house_tree.add_house("HOME")
         await self.house_tree.root.expand()
 
-        self.rseperator = self.lseperator = "\n" * percent(
-            12, y
-        ) + "┃\n" * percent(75, y)
+        self.rseperator = self.lseperator = "\n" * percent(12, y) + "┃\n" * percent(
+            75, y
+        )
 
         self.house_tree_scroll = ScrollView(self.house_tree)
-        self.member_list_scroll = ScrollView(
-            self.member_lists[self.current_house]
-        )
+        self.member_list_scroll = ScrollView(self.member_lists[self.current_house])
 
         await self.populate_local_data()
         await self.refresh_screen()
@@ -367,13 +281,9 @@ class Tui(App):
 
         for message in self.client.chats:
             if message.action == "push_text":
-                await self.perform_push_text(
-                    message, local=True
-                )
+                await self.perform_push_text(message, local=True)
             else:
-                await eval(
-                    f"self.perform_{message.action}(message)"
-                )
+                await eval(f"self.perform_{message.action}(message)")
 
         self.client.start_connection()
         self.set_interval(0.1, self.server_listen)
@@ -405,9 +315,7 @@ class Tui(App):
         x, y = os.get_terminal_size()
 
         if self.current_screen not in self.chat_scroll:
-            self.chat_scroll[
-                self.current_screen
-            ] = ScrollView(
+            self.chat_scroll[self.current_screen] = ScrollView(
                 self.chat_screen[self.current_screen],
                 gutter=(0, 1),
             )
@@ -420,9 +328,7 @@ class Tui(App):
 
         # self.member_list_scroll = ScrollView(self.member_lists[self.current_house])
         if self.current_house not in self.member_scrolls:
-            self.member_scrolls[
-                self.current_house
-            ] = ScrollView(
+            self.member_scrolls[self.current_house] = ScrollView(
                 self.member_lists[self.current_house]
             )
 
@@ -436,9 +342,7 @@ class Tui(App):
         )
 
         await self.view.dock(self.headbar, name="headbar")
-        await self.member_lists[
-            self.current_house
-        ].root.expand()
+        await self.member_lists[self.current_house].root.expand()
 
         # RIGHT WIDGETS
         if self.current_house != "HOME":
@@ -487,68 +391,56 @@ class Tui(App):
             size=percent(10, y),
             name="input_box",
         )
-        self.refresh(
-            layout=True
-        )  # A little bit too cautious
+        self.refresh(layout=True)  # A little bit too cautious
 
-    async def update_chat_screen(
-        self, house: str, room: str
-    ):
+    async def update_chat_screen(self, house: str, room: str):
         """
         Update the screen when the chat is changed
         """
         # TODO: Make this reactive
 
-        if (
-            self.current_house == house
-            and self.current_room == room
-        ):
+        if self.current_house == house and self.current_room == room:
             return
 
         self.current_house = house
         self.current_room = room
-        self.current_screen = (
-            f"{self.current_house}/{self.current_room}"
-        )
+        self.current_screen = f"{self.current_house}/{self.current_room}"
 
-        self.house_tree.change_room_data(
-            house, room, "pending", "0"
-        )
+        self.house_tree.change_room_data(house, room, "pending", "0")
         self.banner.set_text(self.current_screen)
-        self.house_tree.select(
-            self.current_house, self.current_room
-        )
+        self.house_tree.select(self.current_house, self.current_room)
 
         await self.refresh_screen()
 
-    async def handle_tree_click(
-        self, click: TreeClick
-    ) -> None:
+    async def handle_tree_click(self, click: TreeClick) -> None:
         """
         Handles various clicks
         """
         node = click.node
-        match node.data.type:
-            case "room":
-                if node.parent:
-                    house = str(node.parent.label)
-                    room = str(node.label)
-                    await self.update_chat_screen(
-                        house,
-                        room,
-                    )
-            case "house" | "rank":
-                await node.toggle()
-                self.refresh(layout=True)
+        try: # TODO: message clicks
+            match node.data.type:
+                case "room":
+                    if node.parent:
+                        house = str(node.parent.label)
+                        room = str(node.label)
+                        await self.update_chat_screen(
+                            house,
+                            room,
+                        )
+                case "house" | "rank":
+                    await node.toggle()
+                    self.refresh(layout=True)
 
-            case "member":
-                name = str(node.label)
-                if name == self.user:
-                    return
+                case "member":
+                    name = str(node.label)
+                    if name == self.user:
+                        return
 
-                await self.house_tree.add_room("HOME", name)
-                await self.house_tree.expand_home()
-                await self.update_chat_screen("HOME", name)
+                    await self.house_tree.add_room("HOME", name)
+                    await self.house_tree.expand_home()
+                    await self.update_chat_screen("HOME", name)
+        except:
+            pass
 
     async def action_reset_focus(self):
         await self.headbar.focus()
